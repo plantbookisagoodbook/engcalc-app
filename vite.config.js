bbfffabface0c7ab4ac4051bcd5d7e3b3c8b98a3
Nwene Ogwu <nwene.ogwu@signalenergy.com>
2025-08-11 22:24:06 -0400

    Fixed index.html merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 4d0904931733e4a93d752e29d8c4edfd75d3f0df
 })
